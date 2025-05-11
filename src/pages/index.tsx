b34b16c27525deb0f9944f2e96ef85eb279f5077
Mateus Magalhães <mateusmagalhaesemidio@gmail.com>
2025-05-11 20:44:52 -0300

    [feat]Create shadcn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 
 export default function Home() {
   return (
-        <h2 className="text-xl">Hello world</h2>
+        <Button variant='outline'>
+          Button
+        </Button>
   );
 }
