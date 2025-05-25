b0da9b4653f7b1ee41732c1059d96a1c8bdb360f
Mateus Magalhães <mateusmagalhaesemidio@gmail.com>
2025-05-25 11:27:14 -0300

    [feat]develop layout

---
+++ src/components/active-link/active-link.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+
+type ActiveLink = {
+  children: React.ReactNode;
+} & LinkProps;
+
+export function ActiveLink({ children, href, ...rest }: ActiveLink) {
+  const router = useRouter();
+  const isCurrentPath = router.asPath === href || router.asPath === rest.as;
+
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "text-action-sm transition-colors hover:text-blue-200",
+        isCurrentPath ? "text-blue-200" : "text-gray-100"
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
---
+++ src/components/hero-section/hero-section.tsx
@@ -0,0 +1,11 @@
+export function HeroSection() {
+    return (
+        <section className="mt-16">
+            <div>
+                <div>
+                    <h1>Venda seus produtos como afiliado em um único lugar</h1>
+                </div>
+            </div>
+        </section>
+    )
+}
