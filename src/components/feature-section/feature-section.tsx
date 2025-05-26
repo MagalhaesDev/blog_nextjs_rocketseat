7bcf35fe031dda02d8c670c699e86b3fccef2b84
Mateus Magalhães <mateusmagalhaesemidio@gmail.com>
2025-05-26 20:15:22 -0300

    [feat]Hero section

---
+++ src/components/feature-section/feature-section.tsx
@@ -0,0 +1,9 @@
+export function FeatureSection() {
+  return (
+    <section>
+      <h2 className="text-gray-100 text-heading-lg">
+        Crie um catálogo de produtos online em poucos minutos
+      </h2>
+    </section>
+  );
+}
